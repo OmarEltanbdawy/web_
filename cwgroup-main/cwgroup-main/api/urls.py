4434060f6e13fee05eae636a843ec44fe0482e07
OmarEltanbdawy <129180252+OmarEltanbdawy@users.noreply.github.com>
2026-01-08 17:21:40 +0000

    Route unauthenticated nav to login

--- cwgroup-main/cwgroup-main/api/urls.py
+++ cwgroup-main/cwgroup-main/api/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.contrib import admin
-from django.urls import include, path
-from django.http import HttpResponse
+from django.urls import path, re_path
 
 from .views import main_spa
 
 urlpatterns = [
     path('', main_spa),
+    re_path(r'^(items|profile)(/.*)?$', main_spa),
 ]
